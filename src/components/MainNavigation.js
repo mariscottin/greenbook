11c70850a74182f6689226c38834ee75f340d614
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2020-06-23 23:18:07 -0300

    New navbar

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,76 +1,131 @@
 import React, { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-scroll';
-import Logo from '../Assets/img/GreenBookAcademy.Logo.gif';
+import Logo from '../Assets/img/logomenu.gif';
+import Logo2 from '../Assets/img/GreenBookAcademy.Logo.png';
 import EnglishFlag from '../Assets/img/usa.flag.png'
 import SpainFlag from '../Assets/img/spainflag.png'
 import PortugalFlag from '../Assets/img/portugalflag.png'
-
-import Navlinks from './Navlinks';
-import BackDrop from './Backdrop';
-import SideDrawer from './SideDrawer';
+import { useTranslation } from 'react-i18next';
 
 import i18n from '../i18n';
 
 import './MainNavigation.css';
 
+
 const MainNavigation = () => {
+    const [expanded, setExpanded] = useState(false);
+    
+    const toggleNavbar = () => {
+        expanded ? setExpanded(false) : setExpanded(true);
+    }
 
     const changeLanguage = code => {
         i18n.changeLanguage(code);
+        setExpanded(false);
     }
 
-    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-    
-    const openDrawerHandler = () => {
-        setDrawerIsOpen(true);
-    }
-
-    const closeDrawerHandler = () => {
-        setDrawerIsOpen(false);
-    }
-    
-
+    const { t } = useTranslation();
     return (
-        <React.Fragment>
-            {drawerIsOpen && <BackDrop onClick={closeDrawerHandler} />}
-
-            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-                <nav className="main-navigation__drawer-nav">
-                    <Navlinks closeDrawerHandler={closeDrawerHandler}/>
-                </nav>
-            </SideDrawer>
-
-            <div className='main-header'>
-                <div className="main-header__container">
-                    <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
-                        <span />
-                        <span />
-                        <span />
-                    </button>
-                    <div className="main-navigation__title">
-                        <Link
-                            activeClass="active"
-                            to="coverSection"
-                            spy={true}
-                            smooth={true}
-                            duration={700}
-                            className="nav-link">
-                            <img src={Logo} alt="Logo Rhinotech" />
-                        </Link>
-                    </div>
-                    <nav className="main-navigation__header-nav">
-                        <div className="main-navigation__header-links">
-                            <Navlinks closeDrawerHandler={closeDrawerHandler}/>
+        <Navbar collapseOnSelect expand="lg" bg={`${expanded ? 'dark' : 'light'}`} variant={`${expanded ? 'dark' : 'light'}`} fixed="top" expanded={expanded}>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={toggleNavbar}/>
+            <Navbar.Brand>
+                <Link
+                    activeClass="active"
+                    to="coverSection"
+                    spy={true}
+                    smooth={true}
+                    duration={700}
+                    className="navbar-brand-img"
+                    onClick={() => setExpanded(false)}
+                >
+                    <img src={`${expanded ? Logo : Logo2}`} alt="GreenBook Academy Logo" />
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav>
+                    <Link
+                        activeClass="active"
+                        to="approachSection"
+                        spy={true}
+                        smooth={true}
+                        duration={700}
+                        className="nav-link"
+                        onClick={() => setExpanded(false)}
+                    >
+                        {t('header-1')}
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="aboutSection"
+                        spy={true}
+                        smooth={true}
+                        duration={700}
+                        className="nav-link"
+                        onClick={() => setExpanded(false)}
+                    >
+                        {t('header-2')}
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="methodSection"
+                        spy={true}
+                        smooth={true}
+                        duration={700}
+                        className="nav-link last-nav-link"
+                        onClick={() => setExpanded(false)}
+                    >
+                        {t('header-3')}
+                    </Link>
+                    <div className="mobile-language-options">
+                        {i18n.language !== 'en' &&
+                        <div className="mobile-language__container" onClick={()=>{changeLanguage('en')}}>
+                            <span>EN <img src={EnglishFlag} alt="English"/></span>
                         </div>
-                    </nav>
-                    <div className="country-flags__container">
-                        <div className="english" onClick={()=> changeLanguage('en')}>EN<span><img src={EnglishFlag} alt="English"/></span></div>
-                        <div className="spanish" onClick={()=> changeLanguage('es')}>ES<span><img src={SpainFlag} alt="English"/></span></div>
-                        <div className="portuguese" onClick={()=> changeLanguage('po')}>PR<span><img src={PortugalFlag} alt="English"/></span></div>
+                        }
+                        {i18n.language !== 'es' &&
+                        <div className="mobile-language__container" onClick={()=>{changeLanguage('es')}}>
+                            <span>ES <img src={SpainFlag} alt="Español"/></span>
+                        </div>
+                        }
+                        {i18n.language !== 'po' &&
+                        <div className="mobile-language__container" onClick={()=>{changeLanguage('po')}}>
+                            <span>PR <img src={PortugalFlag} alt="Português"/></span>
+                        </div>
+                        }
                     </div>
-                </div>
+                    <div className="language-options">
+                        <div className="language__container" onClick={()=>{changeLanguage('en')}}>
+                            <span>{i18n.language === 'en' ? <b>EN</b> : 'EN'}
+                                 <img src={EnglishFlag} alt="English"/>
+                            </span>
+                        </div>
+                        <div className="language__container" onClick={()=>{changeLanguage('es')}}>
+                            <span>{i18n.language === 'es' ? <b>ES</b> : 'ES'}
+                                <img src={SpainFlag} alt="Español"/>
+                            </span>
+                        </div>
+                        <div className="language__container" onClick={()=>{changeLanguage('po')}}>
+                            <span>{i18n.language === 'po' ? <b>PR</b> : 'PR'} 
+                                <img src={PortugalFlag} alt="Português"/>
+                            </span>
+                        </div>
+                    </div>
+                </Nav>
+            </Navbar.Collapse>
+            <div className="current-language__container">
+                {i18n.language === 'en' &&
+                    <span style={{color: `${expanded ? 'white' : '#2d2d2d'}`}}>EN <img src={EnglishFlag} alt="English"/></span>
+                }
+                {i18n.language === 'es' &&
+                    <span style={{color: `${expanded ? 'white' : '#2d2d2d'}`}}>ES <img src={SpainFlag} alt="Español"/></span>
+                }
+                {i18n.language === 'po' &&
+                    <span style={{color: `${expanded ? 'white' : '#2d2d2d'}`}}>PR <img src={PortugalFlag} alt="Português"/></span>
+                }
             </div>
-        </React.Fragment>
+        </Navbar>
     );
 };
 
