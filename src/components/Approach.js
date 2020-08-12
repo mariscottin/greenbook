397cbed27b078b81ee9940ddcdfce2b0214dd3b9
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2020-08-12 20:58:11 -0300

    styling fixes

--- src/components/Approach.js
+++ src/components/Approach.js
@@ -10,8 +10,7 @@ const Approach = () => {
             <div className="approach__container">
                 <div className="approach__col-2">
                     <h4>{t('approach-1')}</h4>
-                    <h1>{t('approach-2')}</h1>
-                    <h2>{t('approach-3')}</h2>
+                    <h1>{t('approach-2')}<span> {t('approach-3')}</span></h1>
                 </div>
             </div>
         </div>
