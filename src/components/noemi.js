99c5eebd1dfa99fea876e9878c74e5c032ba0654
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2020-12-11 15:04:39 -0300

    some fixes

--- src/components/noemi.js
+++ src/components/noemi.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 import noemiimg from '../Assets/img/MIMI-1.png';
 
+import './Noemi.css';
 function Noemi() {
+    const { t } = useTranslation();
     return (
         <div className="about__section" id="noemiSection">
             <div className="about__container">
